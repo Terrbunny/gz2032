07eaa4ef4300c1e00234bfe7659a6f8102e77ae4
Terrbunny <164021695@qq.com>
2020-08-15 10:38:20 +0800

    Terrbunny新加了一句话

--- 01-git程序/src/com/gec/test/Test1.java
+++ 01-git程序/src/com/gec/test/Test1.java
@@ -3,7 +3,8 @@ package com.gec.test;
 public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("TAT");
+		
+		//Terrbunny新加了一句话
+		System.out.println("QAQ");
 	}
-	
-
 }
